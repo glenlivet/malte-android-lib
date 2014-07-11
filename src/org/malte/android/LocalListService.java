bb8ed9f9f0de66d1fe344212b62b91d96618b0b6
shulai.zhang <kikyou1986@hotmail.com>
2014-07-11 18:02:08 +0800

    remove useless declaration in LocalListService

--- src/org/malte/android/LocalListService.java
+++ src/org/malte/android/LocalListService.java
@@ -5,16 +5,5 @@ public abstract class LocalListService<T> extends LocalService {
 
 	@Override
 	protected abstract ListViewContent<T> getViewContent();
-
-	@Override
-	public abstract void updateViewContent();
-
-	@Override
-	public abstract void onHttpFailure(Integer httpCode);
-
-	@Override
-	public abstract void onHttpSuccess(String sm);
 	
-	
-
 }
